7e05ed7e8554ffb01c65659eab4b8d43d8e32683
maxim <maxim.ermishkin37@gmail.com>
2016-05-29 14:01:26 +0300

    test

--- task-fuzbuz.js
+++ task-fuzbuz.js
@@ -21,7 +21,7 @@ console.clear();
         var index = -1;
         
         // поиск всех вхождений
-        while ((index = s.indexOf(substr, index + 1)) != -1) {
+        while ((index = s.indexOf(substr, index + substr.length)) != -1) {
 //        while (index != s.length) {
             
 //            index++;
